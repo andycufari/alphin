96ea3d386346deac90c639bda642ff49b6c3ed1f
JuanCasimiro <juanyc434@gmail.com>
2025-03-22 20:04:50 -0300

    fix contrato governator

--- contracts/AlphinGovernor.sol
+++ contracts/AlphinGovernor.sol
@@ -5,9 +5,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
-contract AlphinGovernor is Governor, GovernorCountingSimple, GovernorVotes, ERC2771Context {
+contract AlphinGovernor is Governor, GovernorCountingSimple, GovernorVotes {
     uint256 public constant VOTING_DELAY = 1;
     uint256 public constant VOTING_PERIOD = 50400;
     uint256 public constant PROPOSAL_THRESHOLD = 0;
@@ -16,7 +15,7 @@ contract AlphinGovernor is Governor, GovernorCountingSimple, GovernorVotes, ERC2
     constructor(
         IVotes _token,
         address trustedForwarder
-    ) Governor("Alphin Governor") GovernorVotes(_token) ERC2771Context(trustedForwarder) {}
+    ) Governor("Alphin Governor") GovernorVotes(_token) {}
 
     function votingDelay() public pure override returns (uint256) {
         return VOTING_DELAY;
@@ -34,28 +33,6 @@ contract AlphinGovernor is Governor, GovernorCountingSimple, GovernorVotes, ERC2
         uint256 blockNumber
     ) public pure override returns (uint256) {
         return QUORUM_PERCENTAGE;
-    }
-
-    // Override _msgSender() and _msgData() to use ERC2771Context
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address sender) {
-        return ERC2771Context._msgSender();
-    }
-
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
-        return ERC2771Context._msgData();
-    }
-
-    // Override voting functions to use _msgSender()
-    function castVote(uint256 proposalId, uint8 support) public override returns (uint256) {
-        return super.castVote(proposalId, support, _msgSender());
-    }
-
-    function castVoteWithReason(uint256 proposalId, uint8 support, string memory reason) public override returns (uint256) {
-        return super.castVoteWithReason(proposalId, support, reason, _msgSender());
-    }
-
-    function castVoteBySig(uint256 proposalId, uint8 support, uint8 v, bytes32 r, bytes32 s) public override returns (uint256) {
-        return super.castVoteBySig(proposalId, support, v, r, s, _msgSender());
     }
 
 }
