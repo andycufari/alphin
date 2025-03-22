c71d72227455ce6055c0a9df3fd9624e0a6d1a7c
JuanCasimiro <juanyc434@gmail.com>
2025-03-22 13:43:36 -0300

    fix contracts

---
+++ contracts/AlphinERC20.sol
@@ -0,0 +1,34 @@
+// contracts/CATToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract AlphinToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() 
+        ERC20("Alphin Token", "APH") 
+        ERC20Permit("Alphin") 
+    {
+        // Mint inicial: 1M tokens
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    // The following functions are overrides required by Solidity
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
---
+++ contracts/AlphinGovernor.sol
@@ -0,0 +1,40 @@
+// contracts/CATGovernor.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+
+contract AlphinGovernor is Governor, GovernorCountingSimple, GovernorVotes {
+    uint256 public constant VOTING_DELAY = 1;
+    uint256 public constant VOTING_PERIOD = 50400;
+    uint256 public constant PROPOSAL_THRESHOLD = 0;
+    uint256 public constant QUORUM_PERCENTAGE = 20;
+
+    constructor(IVotes _token)
+        Governor("Alphin Governor")
+        GovernorVotes(_token)
+    {}
+
+    function votingDelay() public pure override returns (uint256) {
+        return VOTING_DELAY;
+    }
+
+    function votingPeriod() public pure override returns (uint256) {
+        return VOTING_PERIOD;
+    }
+
+    function proposalThreshold() public pure override returns (uint256) {
+        return PROPOSAL_THRESHOLD;
+    }
+
+    function quorum(uint256 blockNumber) 
+        public 
+        pure 
+        override 
+        returns (uint256) 
+    {
+        return QUORUM_PERCENTAGE;
+    }
+}
