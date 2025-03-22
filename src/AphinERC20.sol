69f0510f29246ea8e16a49a9fe875b350edffa6f
JuanCasimiro <juanyc434@gmail.com>
2025-03-22 12:20:57 -0300

    Contrato ERC20

---
+++ src/AphinERC20.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AlfinToken is ERC20Votes, Ownable {
+    uint256 public constant INITIAL_SUPPLY = 1000000 * 10**18; // 1 millón de tokens
+    mapping(address => bool) public hasJoined;
+
+    constructor() ERC20("AlfinDAO Token", "ALF") ERC20Permit("AlfinDAO Token") {
+        _mint(msg.sender, INITIAL_SUPPLY); // Admin recibe todos los tokens al inicio
+    }
+
+    function joinDAO() external {
+        require(!hasJoined[msg.sender], "Already joined");
+        uint256 tokensToMint = 100 * 10**18; // 100 ALF por usuario nuevo
+        _mint(msg.sender, tokensToMint);
+        _delegate(msg.sender, msg.sender); // Activa el spoder de voto
+
+        hasJoined[msg.sender] = true;
+    }
+}
